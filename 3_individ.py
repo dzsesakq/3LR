3aa1568a910bfeb9eff75660f9e9d89ea15ba4b9
DZSESAKQ <jahongirabdikadirov@gmail.com>
2022-06-08 19:27:00 +0300

    3_individ

---
+++ 3_individ.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    for i in range(1, 10):
+        for j in range(8, 10):
+            for k in range(0, 10):
+                if i + j + k == i * j * k:
+                    print(f"{i}{j}{k}")
+
+
+
+
+            
+
+
+
